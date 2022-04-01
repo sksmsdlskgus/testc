b60c0feb5ed0845b9e2d6a5347cb467182a2b257
sksmsdlskgus <102345450+sksmsdlskgus@users.noreply.github.com>
2022-04-01 15:16:26 +0900

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -1,13 +1,19 @@
 #include <stdio.h>
 main( ) {
 	int i;
-	float sum=0;
-	float hap=0;
+	float sum,sum1=0;
+	float hap,hap1=0;
+	
 	for(i=8; i<50; i=i+2)
 	{
 	hap = hap+(i+2.0);
-	sum = sum+ (float)i/hap; }
+	sum = sum+hap; }
 	
-	printf("sum = %f ", sum);
-}
+	for(i=2; i<48; i=i+2) {	
+	hap1 = hap1+(i+2.0);
+	sum1 = sum1+ hap1; 
+	}
 
+	
+	printf("result = %f ", sum/sum1);
+}

c708ed29261e47fff57e25d0988b3c49038bcb30
sksmsdlskgus <102345450+sksmsdlskgus@users.noreply.github.com>
2022-04-15 14:00:43 +0900

    Add files via upload

--- mytest.c
+++ mytest.c
@@ -1,10 +1,32 @@
 #include <stdio.h>
-main( ) {
+main(){
+
+
+char hh[] = "We are the champion";
+char Large[80];
+char Small[80];
   int i;
-  float sum=0;
-  for(i=2; i<11; i=i++)
+    int c=0;
+    printf("%s\n", hh); //we are the champion
     
-	 sum=sum+(i+3.0)/(i*(i++));
-	   
-	printf(" sum = %f ",sum);
+    for(i=0; hh[i]!='\0';i++){ 
+     Large[i]=hh[i];
+	if (Large[i]>='A'&& Large[i]<='Z')
+	    printf("%c",Large[i]);
+	    else if (Large[i]>='a'&& Large[i]<='z')
+	     printf("%c",Large[i] - ('a'-'A'));
+	     
+		 else 
+	     printf("%c",Large[i]);
+	  }   //WE ARE THE CHAMPION
+       
+	  
+	  
+	    for(i=0;hh[i]!='\0'; i++) 
+	    	Small[i]=hh[i];
+		 if (Small[i]>='a'&& Small[i]<='z')
+	     
+		
+        printf("\n%s\n",Small);	 //we are the champion
+
 }
